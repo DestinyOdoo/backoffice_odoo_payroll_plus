43d8890d0e3dd4331fa768bdaecc5619a226664d
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-05-29 14:29:26 -0500

    [ADD] Add manuals and final advance

--- hr_employment_certificate_client/pyarmor_runtime_011791/__init__.py
+++ hr_employment_certificate_client/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-14T16:53:20.909334
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:21:50.651024
 from odoo.addons.hr_employment_certificate_client.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
