15f567555b00ae96ed04c59e0233bff03a06013e
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-05-29 14:53:45 -0500

    [DEL] Delete manuals before version2

--- hr_payroll_income_withholding_client/pyarmor_runtime_011791/__init__.py
+++ hr_payroll_income_withholding_client/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:21:50.651024
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:50:44.651541
 from odoo.addons.hr_payroll_income_withholding_client.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
