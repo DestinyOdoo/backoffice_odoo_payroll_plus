5adf2a2ed085668c0a560e2c110508b1f835283f
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-06-03 13:31:09 -0500

    [ADD] Add all modules to work QA

--- hr_contract_completion_client/pyarmor_runtime_011791/__init__.py
+++ hr_contract_completion_client/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-14T16:53:20.909334
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:58:49.010043
 from odoo.addons.hr_contract_completion_client.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
